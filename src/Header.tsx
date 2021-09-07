a3aa1c072c77a1a429ebdf65c26ecdfac3d12917
Park Seonu <cotwo0139@gmail.com>
2021-09-07 23:03:32 +0900

    feat(Header): add Header component (WIP)

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Button, Flex, Spacer, Select } from '@chakra-ui/react';
-import { MoonIcon, SunIcon, SearchIcon } from '@chakra-ui/icons';
+import { Button, Flex, Spacer, Menu, IconButton, Text } from '@chakra-ui/react';
+import { MoonIcon, SunIcon, SearchIcon, ArrowForwardIcon, RepeatIcon } from '@chakra-ui/icons';
 
 function Header() {
+  const dark = false//mock
   return (
     <Flex bg="blackAlpha.100" w="100%" p={4}>
-      <Button>Heliotrope</Button>
+      <Button onClick={()=> window.open("https://github.com/Saebasol/", "_blank")} mr={1}><Text fontSize="md">Heliotrope</Text></Button>
+      <Button mr={2} aria-label="Random" leftIcon={<RepeatIcon />}><Text fontSize="sm">랜덤 작품</Text></Button>
       <Button><SearchIcon /></Button>
       <Spacer />
-      <Button><MoonIcon /></Button>
+      <IconButton mr={1} aria-label={dark ? "Enable Light mode" : "Enable Dark mode"}>{dark ? <MoonIcon /> : <SunIcon />}</IconButton>
+      <IconButton aria-label="Login button" icon={<ArrowForwardIcon />}></IconButton>
     </Flex>
   );
 }
