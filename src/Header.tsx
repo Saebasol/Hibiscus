b23ac07e4a7e07078b496d1d500dde67da1860be
Park Seonu <cotwo0139@gmail.com>
2021-09-08 20:26:22 +0900

    refactor: lint files & style

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Button,
   Flex,
-  Center,
   Spacer,
   Avatar,
   IconButton,
@@ -19,13 +18,13 @@ import {
 } from '@chakra-ui/icons';
 
 function Header() {
-  const [ textVisible ] = useMediaQuery('(min-width: 720px)');
+  const [textVisible] = useMediaQuery('(min-width: 720px)');
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Flex bg="blackAlpha.100" w="100%" p={4}>
       <Button
         variant={textVisible ? 'ghost' : 'link'}
-        onClick={() => window.location.href = '/'}
+        onClick={() => (window.location.href = '/')}
         mr={1}
       >
         <Avatar
@@ -33,7 +32,7 @@ function Header() {
           src="https://avatars.githubusercontent.com/u/73676374?s=200&v=4"
           mr={textVisible ? 2 : 0}
         />
-        {textVisible ? <Text fontSize="sm">Heliotrope-web</Text> : ""}
+        {textVisible ? <Text fontSize="sm">Heliotrope-web</Text> : ''}
       </Button>
       <IconButton mr={1} aria-label="Random" icon={<RepeatIcon />} />
       <IconButton aria-label="Search" icon={<SearchIcon />} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import { ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ColorModeScript } from '@chakra-ui/react';
 import App from './App';
 
 ReactDOM.render(
