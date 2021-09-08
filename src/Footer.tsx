18a4ef7fe5869e2236b4445ca7e4f836b1cd8340
Park Seonu <cotwo0139@gmail.com>
2021-09-08 21:58:37 +0900

    feat(Footer): add component Footer

---
+++ src/Footer.tsx
@@ -0,0 +1,7 @@
+import { Flex } from '@chakra-ui/react';
+import * as React from 'react';
+
+function Footer() {
+  return <Flex as="footer" bg="blackAlpha.100" w="100%" p={4}></Flex>;
+}
+export default Footer;
