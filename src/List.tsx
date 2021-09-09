e08a316bcab4c60d833c225c76b5d9e40e802b5d
Park Seonu <cotwo0139@gmail.com>
2021-09-09 21:20:37 +0900

    refactor(lint): lint files

--- src/List.tsx
+++ src/List.tsx
@@ -1,56 +1,47 @@
-import { Container} from '@chakra-ui/react'
-import * as React from 'react'
-import api from './api'
-import Info from './Info'
-import type { heliotropeList, heliotropeInfo } from './types'
+import { Container } from '@chakra-ui/react';
+import * as React from 'react';
+import api from './api';
+import Info from './Info';
+import type { heliotropeList, heliotropeInfo } from './types';
 
 const initInfo: heliotropeInfo = {
-    title: "title",
-    thumbnail: "",
-    artist: [{value: "value", url:"url"}],
-    group: [{value: "value", url:"url"}],
-    type: null,
-    language: null,
-    series: [{value: "value", url:"url"}],
-    character: [{value: "value", url:"url"}],
-    tags: [{value: "value", url:"url"}],
-    date: "date"
-}
-
+  title: 'title',
+  thumbnail: '',
+  artist: [{ value: 'value', url: 'url' }],
+  group: [{ value: 'value', url: 'url' }],
+  type: null,
+  language: null,
+  series: [{ value: 'value', url: 'url' }],
+  character: [{ value: 'value', url: 'url' }],
+  tags: [{ value: 'value', url: 'url' }],
+  date: 'date',
+};
 
 const List = () => {
+  const { useState, useEffect } = React;
+  const [info, setInfo] = useState<heliotropeInfo[]>([initInfo]);
+  const [loading, setLoading] = useState(false);
 
-    const { useState, useEffect } = React
-    const [info, setInfo] = useState<heliotropeInfo[]>([initInfo])
-    const [loading, setLoading] = useState(false)
+  const fetchHeliotropeList = async () => {
+    const response = await fetch(api + '/hitomi/list/1');
+    const heliotropeListResponse: heliotropeList = await response.json();
 
+    const info = heliotropeListResponse.list;
 
-    const fetchHeliotropeList = async () => {
-        const response = await fetch(api + "/hitomi/list/1")
-        const heliotropeListResponse: heliotropeList = await response.json()
+    setInfo(info);
+    setLoading(true);
+  };
 
-        const info = heliotropeListResponse.list
+  useEffect(() => {
+    fetchHeliotropeList();
+  }, []);
 
-        setInfo(info)
-        setLoading(true)
-    
-    }
-
-    useEffect(() => {
-        fetchHeliotropeList()
-    }, [])
-
-    return (
-        // 컨테이너 나중에 옮겨야함
-        <Container 
-            w="100%" 
-            maxW={{lg: "1140px"}}
-            p={4}
-        >
-            {loading ? info.map(e => <Info {...e}/>) : "로딩중..."}
-        </Container>
-    )
-}
-
+  return (
+    // 컨테이너 나중에 옮겨야함
+    <Container w="100%" maxW={{ lg: '1140px' }} p={4}>
+      {loading ? info.map((e) => <Info {...e} />) : '로딩중...'}
+    </Container>
+  );
+};
 
 export default List;
