6da6b475c76ef0fee45eac83f144568456ccaaa1
Ryu JuHeon <saidbysolo@gmail.com>
2021-09-09 20:26:08 +0900

    refactor: implement list and export info

--- src/List.tsx
+++ src/List.tsx
@@ -1,11 +1,12 @@
-import { Flex, Box, Image, Text, Link, Container, Center} from '@chakra-ui/react'
+import { Container} from '@chakra-ui/react'
 import * as React from 'react'
 import api from './api'
+import Info from './Info'
 import type { heliotropeList, heliotropeInfo } from './types'
 
-const initInfo = {
+const initInfo: heliotropeInfo = {
     title: "title",
-    thumbnail: "https://media.discordapp.net/attachments/745844596176715806/885093528424833024/92565412_p0_master1200.png",
+    thumbnail: "",
     artist: [{value: "value", url:"url"}],
     group: [{value: "value", url:"url"}],
     type: null,
@@ -13,26 +14,30 @@ const initInfo = {
     series: [{value: "value", url:"url"}],
     character: [{value: "value", url:"url"}],
     tags: [{value: "value", url:"url"}],
-    date: "date",
+    date: "date"
 }
+
 
 const List = () => {
 
     const { useState, useEffect } = React
-    const [info, setInfo] = useState<heliotropeInfo>(initInfo)
+    const [info, setInfo] = useState<heliotropeInfo[]>([initInfo])
+    const [loading, setLoading] = useState(false)
 
 
-    // const fetchHeliotropeList = async () => {
-    //     const response = await fetch(api + "/hitomi/list/1")
-    //     const heliotropeListResponse: heliotropeList = await response.json()
+    const fetchHeliotropeList = async () => {
+        const response = await fetch(api + "/hitomi/list/1")
+        const heliotropeListResponse: heliotropeList = await response.json()
 
-    //     const info = heliotropeListResponse.list[0]
-    //     setInfo(info)
+        const info = heliotropeListResponse.list
+
+        setInfo(info)
+        setLoading(true)
     
-    // }
+    }
 
     useEffect(() => {
-
+        fetchHeliotropeList()
     }, [])
 
     return (
@@ -42,75 +47,7 @@ const List = () => {
             maxW={{lg: "1140px"}}
             p={4}
         >
-            <Box 
-                display={{ md: "flex" }}
-                padding="0.3125rem" 
-                borderRadius="0.1875rem"
-                boxShadow="rgb(0 0 0 / 16%) 0px 0.1875rem 0.1875rem 0px, rgb(0 0 0 / 8%) 0px 0px 0px 0.0625rem"
-                border="0.0625rem solid rgba(0,0,0, 0.16)" 
-                alignItems="stretch" 
-                marginBottom="1.5rem"
-             >
-                <Center
-                    backgroundColor="rgb(34, 36, 38)" 
-                    flexShrink={0}
-                >
-                    <Image
-                        maxW="100%" 
-                        maxH="300px"
-                        width={{ md: 40 }}
-                        alt="thumbnail"
-                        src={info.thumbnail}
-                    />
-                </Center>
-                <Box 
-                    mt={{ base: 4, md: 0 }}
-                    ml={{ md: 6 }}
-                >
-                    <Link
-                        mt={1}
-                        display="block"
-                        fontSize="lg"
-                        lineHeight="normal"
-                        fontWeight="semibold"
-                        // 여기에 reader 리다이렉트
-                        href="#"
-                    >
-                        {info.title}
-                    </Link>
-                    <Text
-                        // 최적화가 필요한곳.
-                        mt={2} 
-                        color="gray.500"
-                    >
-                        작가: {info.artist.map(e => e.value)}
-                    </Text>
-                    <Text 
-                        mt={2} 
-                        color="gray.500"
-                    >
-                        원작: {info.series.map(e => e.value)}
-                    </Text>
-                    <Text 
-                        mt={2} 
-                        color="gray.500"
-                    >
-                        종류: {info.type ? info.series.map(e => e.value) : "N/A"}
-                    </Text>
-                    <Text 
-                        mt={2} 
-                        color="gray.500"
-                    >
-                        캐릭터: {info.series.map(e => e.value)}
-                    </Text>
-                    <Text 
-                        mt={2} 
-                        color="gray.500"
-                    >
-                        태그: {info.tags.map(e => e.value)}
-                    </Text>
-                </Box>
-            </Box>
+            {loading ? info.map(e => <Info {...e}/>) : "로딩중..."}
         </Container>
     )
 }
