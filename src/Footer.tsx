5e3c15b2ad1b5923b47a116642e4ca9530734680
Park Seonu <cotwo0139@gmail.com>
2021-09-09 21:20:53 +0900

    feat(Footer): add footer content

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,38 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Stack, Button, Text, Icon } from '@chakra-ui/react';
+import { FiGithub } from 'react-icons/fi';
+import { SiDiscord } from 'react-icons/si';
 import * as React from 'react';
 
 function Footer() {
-  return <Flex as="footer" bg="blackAlpha.100" w="100%" p={4}></Flex>;
+  const repo = (): Window | null =>
+    window.open('https://github.com/Saebasol/heliotrope-web', '__blank');
+  const discord = (): Window | null =>
+    window.open('https://discord.gg/PSshFYr', '__blank');
+  return (
+    <Flex as="footer" bg="blackAlpha.100" w="100%" p={4}>
+      <Stack>
+        <Button
+          ml={3}
+          variant="link"
+          onClick={repo}
+          style={{ textDecoration: 'none' }}
+        >
+          <Icon as={FiGithub} mr={3} />
+          <Text>Saebasol/heliotrope-web</Text>
+        </Button>
+      </Stack>
+      <Stack>
+        <Button
+          ml={3}
+          variant="link"
+          onClick={discord}
+          style={{ textDecoration: 'none' }}
+        >
+          <Icon as={SiDiscord} mr={3} />
+          <Text>Saebasol/Discord</Text>
+        </Button>
+      </Stack>
+    </Flex>
+  );
 }
 export default Footer;
