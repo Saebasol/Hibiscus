663d16f770e048cf45785d6d6dae7460888ab625
Ryu JuHeon <saidbysolo@gmail.com>
2021-09-10 19:06:43 +0900

    feat(Loading): add loading component

---
+++ src/Loading.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@chakra-ui/layout"
+import { Spinner } from "@chakra-ui/react"
+import React from "react"
+
+
+const Loading = () => {
+    return (
+        <Flex alignItems="center" justifyContent="center" minHeight="100vh">
+            <Spinner />
+        </Flex>
+    )
+}
+
+export default Loading
