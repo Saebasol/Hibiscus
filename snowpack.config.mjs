0874983e1a9d1a4a28100ba4f2c495f8942ebb9c
Ryu JuHeon <saidbysolo@gmail.com>
2021-09-11 01:27:01 +0900

    feat: add route

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -17,7 +17,7 @@ export default {
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,34 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Header from './Header';
 import Footer from './Footer';
 import theme from './theme';
+import List from './List';
+import Login from './Login';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import HFContainer from './HFContainer';
 
 function App() {
-  const { useEffect } = React;
-
-  useEffect(() => {}, []);
-
   return (
-    <ChakraProvider theme={theme}>
-      <Header />
-      <Footer />
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider theme={theme}>
+
+      <Switch>
+        <Route exact path="/list">
+          <Redirect to="/list/1" />
+        </Route>
+        <Route exact path="/list/:id">
+          <HFContainer children={<List/>}/>
+        </Route>
+        <Route path="/login" exact component={Login}/>
+      </Switch>
+      </ChakraProvider>
+    </Router>
   );
 }
 
