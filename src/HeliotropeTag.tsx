8bb61484f6a677309b50dea714e57d632850c5fa
Park Seonu <cotwo0139@gmail.com>
2021-09-10 20:19:56 +0900

    feat(HeliotropeTag): edit tag color darker

--- src/HeliotropeTag.tsx
+++ src/HeliotropeTag.tsx
@@ -3,20 +3,20 @@ import type { heliotropeValueUrl } from './types';
 import * as Chakra from '@chakra-ui/react';
 
 function HeliotropeTag({ tag }: { tag: heliotropeValueUrl }) {
-  const { Tag } = Chakra;
+  const { Tag, Text } = Chakra;
   const tagColor = (text: string): string => {
     const female = '♀';
     const male = '♂';
     return text.includes(male)
-      ? 'blue.300'
+      ? 'blue.500'
       : text.includes(female)
-      ? 'red.300'
-      : 'gray.300';
+      ? 'red.500'
+      : 'gray.500';
   };
 
   return (
     <Tag mr={1} mt={1} backgroundColor={tagColor(tag.value)}>
-      {tag.value}
+      <Text textColor="black">{tag.value}</Text>
     </Tag>
   );
 }
