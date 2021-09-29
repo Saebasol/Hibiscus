78a57c59bf293d0cd28890cf5ef3eab1929e5b07
Park Seonu <cotwo0139@gmail.com>
2021-09-29 20:53:40 +0900

    feat(Viewer): implement LazyLoading, Viewer Navbar

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -4,17 +4,24 @@ import api from '../api';
 import * as ReactRouterDom from 'react-router-dom';
 import type { heliotropeImageInfo, heliotropeImageList } from '../types';
 import { Center } from '@chakra-ui/react';
-import LazyViewerImage  from './LazyLoadingImage';
+import LazyViewerImage from './LazyViewerImage';
+import ViewerNav from './ViewerNav';
 import Loading from './Loading';
-// TODO: 가로 채우기, 스크롤, 페이지 보기 설정, 뷰어 헤더 구현, Lazy loading 구현
-// COMPLETE: 세로 채우기
+// TODO: 스크롤, 페이지 보기 설정
+// WIP: 뷰어 헤더 구현, Lazy loading 구현
+// Lazyloading 컴포넌트마다 IntersectionObserver 인스턴스가 생성됨. Viewer컴포넌트에서 관리하도록 바꿀 필요 있음
+// COMPLETE: 세로 채우기, 가로 채우기
 const Viewer = () => {
   const { useEffect, useState } = React;
-  const { Box, Image } = Chakra;
+  const { Box } = Chakra;
   const { useParams } = ReactRouterDom;
+
   const { index } = useParams<{ index: string }>();
+
+  const [isVertical, setVertical] = useState<boolean>(true);
   const [loading, setLoading] = useState(true);
   const [images, setImages] = useState<heliotropeImageInfo[]>([]);
+
   const fetchImages = async (): Promise<void> => {
     const response = await fetch(api + '/hitomi/images/' + index);
     const heliotropeFiles: heliotropeImageList = await response.json();
@@ -22,32 +29,42 @@ const Viewer = () => {
     setLoading(false);
     return;
   };
+
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [ ]);
+
   const fillVertical = {
     maxWidth: '100%',
     maxHeight: '100vh',
     marginLeft: 'auto',
     marginRight: 'auto',
   };
+  const fillHorizontal = {
+    minWidth: '90vw',
+    maxHeight: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  };
+
   const imageElements = images.map((e, index) => (
-    <Image
+    <LazyViewerImage
       key={index}
-      loading="lazy"
-      style={fillVertical}
-      m={0}
-      src={api+'/proxy/' + e.url}
+      style={isVertical ? fillVertical : fillHorizontal}
+      src={api + '/proxy/' + e.url}
+      imageNum={index + 1}
     />
   ));
-  //   align-items: center;
-  //   justify-content: center;
   return (
-    <>
-    {/* <LazyViewerImage /> */}
-    <Center>{loading ? <Loading /> : <Box>{[...imageElements]}</Box>}</Center>
-    </>
+    <Center m={0}>
+      <ViewerNav
+        setVertical={setVertical}
+        isVertical={isVertical}
+        index={index}
+      />
+      {loading ? <Loading /> : <Box>{[...imageElements]}</Box>}
+    </Center>
   );
-}
+};
 
 export default Viewer;
