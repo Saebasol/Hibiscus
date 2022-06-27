e474fc6805d5ce5380e7ee6eba6f4a8e4ad53ebf
Ryu juheon <saidbysolo@gmail.com>
2022-06-27 19:49:48 +0900

    feat: add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './theme'
 import * as ReactDOM from 'react-dom/client'
 import App from './App'
+
 
 const rootElement = document.getElementById('root') as HTMLElement
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+const config: ThemeConfig = { initialColorMode: 'dark', useSystemColorMode: false }
+
+const theme = extendTheme({ config })
+
+export default theme
