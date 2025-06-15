a67f87bc7af6379ff42f6dbe4343515419809d85
Ryu Juheon <saidbysolo@gmail.com>
2025-06-15 15:27:03 +0900

    fix: render date in client

--- src/client/components/Info/Date.tsx
+++ src/client/components/Info/Date.tsx
@@ -1,9 +1,13 @@
 import { CalendarIcon } from "@radix-ui/react-icons"
 import { Flex, Text } from "@radix-ui/themes"
+import { useEffect, useState } from "react"
 
 const InfoDate = ({ date }: { date: string }) => {
-  const formatDate = (dateString: string) => {
-    return new Date(dateString).toLocaleDateString(undefined, {
+  const [formatDate, setFormatDate] = useState("")
+
+  useEffect(() => {
+    const dateObj = new Date(date)
+    const formated = dateObj.toLocaleDateString(undefined, {
       year: 'numeric',
       month: 'long',
       day: 'numeric',
@@ -11,13 +15,14 @@ const InfoDate = ({ date }: { date: string }) => {
       minute: '2-digit',
       hour12: false,
     })
-  }
+    setFormatDate(formated)
+  }, [date])
 
   return (
     <Flex align="center" justify="end" gap="2">
       <CalendarIcon />
       <Text size="2" color="gray">
-        {formatDate(date)}
+        {formatDate}
       </Text>
     </Flex>
   )
